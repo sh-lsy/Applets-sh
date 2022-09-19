23eac746d4546bc4ee0968f0d9a6b831ac4709a2
SH <673545134@qq.com>
2022-09-19 21:02:28 +0800

    feat： demo

---
+++ miniprogram/pages/demo/demo.ts
@@ -0,0 +1,60 @@
+// pages/demo/demo.ts
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    arr: ['arr1', 'arr2', 'arr3'],
+    num: 0
+  },
+  add() {
+    this.setData({
+      num: this.data.num + 1
+    });
+  },
+  del() {
+    
+    this.setData({
+      num: this.data.num - 1
+    });
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {},
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {},
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {},
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {}
+});
